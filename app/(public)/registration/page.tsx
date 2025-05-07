7c4906b305fd36e9df6644a38b4ce4ef4387db45
Ritaz <Chichurin06@mail.ru>
2025-05-07 21:17:34 +0300

    implemented registration using the react-hook-form

--- app/(public)/registration/page.tsx
+++ app/(public)/registration/page.tsx
@@ -1,4 +1,30 @@
+'use client'
+
+import { RegistrationData } from '@/app/src/features/auth/model/types'
+import { useRegistration } from '@/app/src/features/auth/model/useRegistration'
+import { useUserStore } from '@/app/src/shared/model/useUserStore'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
 const page = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<RegistrationData>()
+
+	const setUser = useUserStore(state => state.setUser)
+	const { mutateAsync } = useRegistration()
+
+	const router = useRouter()
+
+	const onSubmit: SubmitHandler<RegistrationData> = async data => {
+		const newUser = await mutateAsync(data)
+		setUser(newUser)
+		router.replace('/login')
+	}
+
 	return (
 		<main className='grid grid-cols-[230px,auto] grid-rows-[150px,auto] mt-24 px-[90px]'>
 			<h1
@@ -18,43 +44,73 @@ const page = () => {
 					предложения.
 				</p>
 
-				<div className='flex flex-col gap-9 mt-16'>
+				<form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-9 mt-16'>
 					<input
-						type='text'
+						{...register('name', { required: 'Это поле не может быть пустым' })}
 						placeholder='Имя'
-						className='block w-[60%] text-secondary  bg-transparent border-b-tertiaryBg border-b-[2px] text-xl pb-3'
+						className='outline-none block w-[60%] text-secondary  bg-transparent border-b-tertiaryBg border-b-[2px] text-xl pb-3'
 					/>
+					{errors.name && <p>{errors.name.message}</p>}
+
 					<input
-						type='text'
+						{...register('password', {
+							required: 'Это поле не может быть пустым',
+							minLength: { value: 8, message: 'Минимальная длина пароля 8 символов' },
+						})}
+						type='password'
 						placeholder='Пароль'
-						className='block w-[60%] text-secondary bg-transparent border-b-tertiaryBg border-b-[2px] text-xl pb-3'
+						className='outline-none block w-[60%] text-secondary bg-transparent border-b-tertiaryBg border-b-[2px] text-xl pb-3'
 					/>
+					{errors.password && <p>{errors.password.message}</p>}
+
 					<input
-						type='text'
+						{...register('email', {
+							required: 'Это поле не может быть пустым',
+							pattern: { value: /^\S+@\S+$/i, message: 'Некорректный email' },
+						})}
 						placeholder='Email'
-						className='block w-[60%] text-secondary bg-transparent border-b-tertiaryBg border-b-[2px] text-xl pb-3'
+						className='outline-none block w-[60%] text-secondary bg-transparent border-b-tertiaryBg border-b-[2px] text-xl pb-3'
 					/>
+					{errors.email && <p>{errors.email.message}</p>}
+
 					<input
-						type='text'
+						{...register('phone_number', {
+							required: 'Это поле не может быть пустым',
+							pattern: { value: /^\+?\d[\d\s\-()]{9,14}$/, message: 'Некорректный номер телефона' },
+						})}
 						placeholder='Номер телефона'
-						className='block w-[60%] text-secondary bg-transparent border-b-tertiaryBg border-b-[2px] text-xl pb-3'
+						className='outline-none block w-[60%] text-secondary bg-transparent border-b-tertiaryBg border-b-[2px] text-xl pb-3'
 					/>
-				</div>
+					{errors.phone_number && <p>{errors.phone_number.message}</p>}
 
-				<div className='flex'>
-					<input type='checkbox' className='rounded-[8px] w-[20px] h-[20px]' />
-					<p className='text-[#535353] mt-7 text-lg '>Согласие на обработку персональных данных</p>
-				</div>
+					<div className='flex'>
+						<input
+							{...register('terms', { required: 'Вы должны согласиться с условиями' })}
+							type='checkbox'
+							className='outline-none rounded-[8px] w-[20px] h-[20px]'
+						/>
+						<label className='text-[#535353] ml-2 text-lg'>
+							Согласен на обработку персональных данных
+						</label>
+						{errors.terms && <p>{errors.terms.message}</p>}
+					</div>
 
-				<button className='mt-12 rounded-[5px] bg-accentBg font-bold text-xl text-[#333333] py-2 w-[320px]'>
-					Зарегистрироваться
-				</button>
+					<button
+						type='submit'
+						className='outline-none mt-5 rounded-[5px] bg-accentBg font-bold text-xl text-[#333333] py-3 w-[320px]'
+					>
+						Зарегистрироваться
+					</button>
+				</form>
 
 				<p className='my-7 text-[#535353] font-medium text-lg'>
 					Аккаунт создан?
-					<span className='text-accent ml-4 py-[3px] border-b-[2px] border-b-accentBg font-bold'>
+					<Link
+						href='/login'
+						className='cursor-pointer text-accent ml-4 py-[3px] border-b-[2px] border-b-accentBg font-bold'
+					>
 						Войти
-					</span>
+					</Link>
 				</p>
 			</article>
 		</main>
